d319a87979b8b8b6210da4af3505b03d044a499f
Claudio Bernasconi <email@claudio-bernasconi.net>
2020-03-26 18:03:38 +0100

    Show rounded subscriber counts for subscriber count > 1000

--- Actions/SubscriberCountAction.cs
+++ Actions/SubscriberCountAction.cs
@@ -10,7 +10,42 @@ namespace streamdeckyoutube.Actions
         protected override async Task OnChannelStatisticsReceived(string context, ChannelListResponse channelData)
         {
             var subscriberCount = channelData.Items[0].Statistics.SubscriberCount;
-            await Manager.SetTitleAsync(context, subscriberCount.ToString());
+            string subscriberCountStr;
+
+            if( subscriberCount < 1000 )
+            {
+                subscriberCountStr = subscriberCount.ToString();
+            }
+            else if( subscriberCount == 1000)
+            {
+                subscriberCountStr = "1000";
+            }
+            else if (subscriberCount < 10_000 )
+            {
+                subscriberCountStr = subscriberCount.Value.ToString().Substring(0, 1) + "." + subscriberCount.Value.ToString().Substring(1, 2) + "K";
+            }
+            else if(subscriberCount < 100_000)
+            {
+                subscriberCountStr = subscriberCount.Value.ToString().Substring(0, 2) + "." + subscriberCount.Value.ToString().Substring(2, 1) + "K";
+            }
+            else if(subscriberCount < 1_000_000)
+            {
+                subscriberCountStr = subscriberCount.Value.ToString().Substring(0, 3) + "K";
+            }
+            else if(subscriberCount < 10_000_000)
+            {
+                subscriberCountStr = subscriberCount.Value.ToString().Substring(0, 1) + "." + subscriberCount.Value.ToString().Substring(1, 2) + "M";
+            }
+            else if(subscriberCount < 100_000_000)
+            {
+                subscriberCountStr = subscriberCount.Value.ToString().Substring(0, 2) + "." + subscriberCount.Value.ToString().Substring(2, 1) + "M";
+            }
+            else
+            {
+                subscriberCountStr = subscriberCount.Value.ToString().Substring(0, 3) + "M";
+            }
+
+            await Manager.SetTitleAsync(context, subscriberCountStr);
         }
     }
 }
